93e3b023a5eeeb9f875cba14c4dbd1f9b7ca201e
Estefani Hernandez <estefanih30521@students.laalliance.org>
2017-11-17 17:40:31 +0000

    Done.

---
+++ countries.js
@@ -0,0 +1,14 @@
+var countries = [
+    "Chad", "Cuba", "Greenland", "Iraq", "Mali", "Oman", "India"
+]
+countries.push("Fiji")
+countries.splice(4, 0, "Iran")
+countries.reverse()
+countries.splice(2, 1, "Togo")
+countries.push("Laos")
+countries.reverse()
+countries.unshift("Peru")
+console.log(countries)
+ 
+
+
